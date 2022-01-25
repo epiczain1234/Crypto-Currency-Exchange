ee5af908fd4081b191343d212be8fa73d26e9d8c
epiczain1234 <mukatizain@gmail.com>
2022-01-25 09:07:03 -0800

    transferring the balance of the deploying address to the ethswap exchange

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+ // Transferring full supply of tokens into the ethswap exchange
+  await token.transfer(ethSwap.address, "1000000000000000000000000");
 };
