17165bb29e918ea0c05af60263a01b8cd78e7851
epiczain1234 <mukatizain@gmail.com>
2022-01-27 21:30:46 -0800

    added a before hook

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -12,24 +12,31 @@ require('chai')
     
 // Ethswap contract
 contract('EthSwap', (accounts) => {
+    let token, ethSwap;
+    // this hok is commonly used to make sure that the prerequisite steps are performed before the rest of the code is executed
+    before(async () => {
+        token = await Token.new();
+        ethSwap = await EthSwap.new();
+        // Transfer all tokens to Ethswap
+        await token.transfer(ethSwap.address, '1000000000000000000000000')
+    })
+
     describe('Token deployment', async () => {
         it('contract has a name', async () => {
-            let token = await Token.new()
             const name = await token.name()
             assert.equal(name, "Zain Token") 
         })
     })
+    // we are using asynchronous functions because we do not want to execute in sequence since we could be wasting a lot of time waiting for 
+    // certain blocks of code to finish executing
     describe('EthSwap deployment', async () => {
         it('contract has a name', async () => {
-            let ethSwap = await EthSwap.new()
             const name = await ethSwap.name()
             assert.equal(name, "EthSwap Instant Exchange") 
         })
     
         it('contract has tokens',async () => {
-            let token = await Token.new();
-            let ethSwap = await EthSwap.new();
-            await token.transfer(ethSwap.address, '1000000000000000000000000')
+            // duplicate initializing of tokens
             let balance = await token.balanceOf(ethSwap.address)
             assert.equal(balance.toString(), '1000000000000000000000000')
         })
